a03bd91406e86dcdef6617f2b2f236ee268f4257
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-03-06 21:12:52 +0800

    demo

---
+++ src/main/java/com/fear/springbootdisplay/SpringbootDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.fear.springbootdisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDisplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fear/springbootdisplay/SpringbootDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fear.springbootdisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
