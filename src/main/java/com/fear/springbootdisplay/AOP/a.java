179e2a98d6e0dbee7e95cc19b14507f96a711131
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-03-10 15:01:21 +0800

    a

---
+++ src/main/java/com/fear/springbootdisplay/AOP/a.java
@@ -0,0 +1,13 @@
+package com.fear.springbootdisplay.AOP;
+
+/**
+ * @author 梁懿豪
+ * @version 1.0
+ * @Github https://github.com/fearlesslyh
+ * @date 2025/3/10 14:41
+ */
+public class a {
+    public synchronized void sizeCount(int size){
+
+    }
+}
